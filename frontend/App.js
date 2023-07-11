1b0178732355ab612c69c95518b29aa656efb91d
ryantangmj <110431837+ryantangmj@users.noreply.github.com>
2023-07-11 15:05:47 +0800

    added filter function in ChatPage

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,12 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useState } from 'react';
 import ChatPage from './screens/ChatPage';
 import HomePage from './screens/HomePage';
 import LoginPage from './screens/LoginPage';
 import RegisterPage from './screens/RegisterPage';
 import LostAndFound from './screens/LostAndFound';
 import ListingPage from './screens/ListingPage';
+import { ChatProvider } from './ChatContext';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -13,17 +14,22 @@ import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [selectedModules, setSelectedModules] = useState([]);
+  const [showAllModules, setShowAllModules] = useState(true);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <ChatProvider value={{ selectedModules, setSelectedModules, showAllModules, setShowAllModules }}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginPage} options={{ title: 'Login' }} />
-        <Stack.Screen name="Register" component={RegisterPage} options={{ title: 'Register' }} />
-        <Stack.Screen name="Home" component={HomePage} options={{ title: 'Home' }} />
-        <Stack.Screen name="Chat" component={ChatPage} options={{ title: 'Module Chats' }} />
-        <Stack.Screen name="LostAndFound" component={LostAndFound} options={{ title: 'Lost And Found' }} />
-        <Stack.Screen name="ListingPage" component={ListingPage} options={{ title: 'Listing Page' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+          <Stack.Screen name="Register" component={RegisterPage} options={{ title: 'Register' }} />
+          <Stack.Screen name="Home" component={HomePage} options={{ title: 'Home' }} />
+          <Stack.Screen name="Chat" component={ChatPage} options={{ title: 'Module Chats' }} />
+          <Stack.Screen name="LostAndFound" component={LostAndFound} options={{ title: 'Lost And Found' }} />
+          <Stack.Screen name="ListingPage" component={ListingPage} options={{ title: 'Listing Page' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ChatProvider>
   );
 };
 
--- frontend/screens/ChatPage.js
+++ frontend/screens/ChatPage.js
@@ -1,29 +1,42 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, View, Text, FlatList, TextInput, KeyboardAvoidingView, TouchableOpacity, SafeAreaView, Alert } from 'react-native';
+import React, { useState, useEffect, useRef, useContext } from 'react';
+import { StyleSheet, View, Text, FlatList, TextInput, KeyboardAvoidingView, TouchableOpacity, SafeAreaView, Modal, ScrollView } from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import Icon from 'react-native-vector-icons/Ionicons';
 import BottomBar from '../BottomBar';
+import { ChatContext } from '../ChatContext';
 import { firestore, auth } from '../firebase';
 
 const ChatPage = ({ navigation }) => {
+  const { selectedModules, setSelectedModules, showAllModules, setShowAllModules } = useContext(ChatContext);
   const [chats, setChats] = useState([]);
   const [currentChat, setCurrentChat] = useState(null);
   const [messageText, setMessageText] = useState('');
   const [username, setUsername] = useState('');
   const [searchText, setSearchText] = useState('');
   const [filteredChats, setFilteredChats] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [moduleOptions, setModuleOptions] = useState([]);
+  const [tempSelectedModules, setTempSelectedModules] = useState([]);
   const flatListRef = useRef(null);
   const currentChatMessages = currentChat?.messages || [];
+
   useEffect(() => {
     const unsubscribe = firestore.collection('chats').onSnapshot(snapshot => {
-      const updatedChats = snapshot.docs.map(doc => {
+      const updatedChats = [];
+      const options = [];
+
+      snapshot.forEach(doc => {
         const { moduleCode, messages } = doc.data();
-        return {
+        updatedChats.push({
           id: doc.id,
           moduleCode,
           messages,
-        };
+        });
+        options.push(moduleCode);
       });
+
       setChats(updatedChats);
+      setModuleOptions(options);
     });
 
     return () => unsubscribe();
@@ -48,6 +61,15 @@ const ChatPage = ({ navigation }) => {
       setFilteredChats(filtered);
     }
   }, [searchText, chats]);
+
+  useEffect(() => {
+    if (showAllModules) {
+      setFilteredChats(chats);
+    } else {
+      const filtered = chats.filter((chat) => selectedModules.includes(chat.moduleCode));
+      setFilteredChats(filtered);
+    }
+  }, [selectedModules, showAllModules, chats]);
 
   const handleChatPress = (chatId) => {
     const chat = chats.find((c) => c.id === chatId);
@@ -77,46 +99,55 @@ const ChatPage = ({ navigation }) => {
     }));
 
     // Scroll to the last message
-    if (currentChatMessages.length > 0){
+    if (currentChatMessages.length > 0) {
       flatListRef.current.scrollToEnd();
     }
   };
 
-  // const handleCreateChat = () => {
-  //   Alert.prompt(
-  //     'Create New Chat',
-  //     'Enter the chat name:',
-  //     (chatName) => {
-  //       if (chatName.trim() !== '') {
-  //         firestore.collection('chats').add({
-  //           title: chatName.trim(),
-  //           messages: [],
-  //         });
-  //       }
-  //     }
-  //   );
-  // };
-
   const renderChatItem = ({ item }) => (
-    <TouchableOpacity style={styles.chatItem} onPress={() => handleChatPress(item.id)}>
+    <TouchableOpacity key={item.id} style={styles.chatItem} onPress={() => handleChatPress(item.id)}>
       <Text style={styles.chatTitle}>{item.moduleCode}</Text>
     </TouchableOpacity>
   );
+
+  const handleFilter = () => {
+    setShowAllModules(selectedModules.length === 0);
+
+    if (modalVisible) {
+      setModalVisible(false);
+    } else {
+      setTempSelectedModules([...selectedModules]);
+      setModalVisible(true);
+    }
+  };
+
+  const applyFilter = () => {
+    setSelectedModules(tempSelectedModules);
+    setShowAllModules(tempSelectedModules.length === 0);
+
+    if (tempSelectedModules.length === 0) {
+      setFilteredChats(chats);
+    } else {
+      const filtered = chats.filter((chat) => tempSelectedModules.includes(chat.moduleCode));
+      setFilteredChats(filtered);
+    }
+
+    setModalVisible(false);
+  };
 
   const renderMessageItem = ({ item, index }) => {
     const isSentByCurrentUser = item.username === username;
     const containerStyle = isSentByCurrentUser
       ? styles.sentMessageContainer
       : styles.receivedMessageContainer;
-    const itemStyle = isSentByCurrentUser
-      ? styles.sentMessageItem
-      : styles.receivedMessageItem;
 
     return (
       <View style={[styles.messageItemContainer, containerStyle]}>
         <View style={styles.messageItem}>
           <Text style={styles.username}>{item.username}</Text>
-          <Text style={styles.messageText}>{item.message}</Text>
+          <View style={styles.messageContent}>
+            <Text style={styles.messageText}>{item.message}</Text>
+          </View>
         </View>
       </View>
     );
@@ -189,15 +220,66 @@ const ChatPage = ({ navigation }) => {
             placeholder="Search chats..."
             value={searchText}
             onChangeText={setSearchText}
-            autoCapitalize='none'
+            autoCapitalize="none"
           />
+          <TouchableOpacity style={styles.filterButton} onPress={handleFilter}>
+            <Icon name="filter" size={25} color="#ffffff" />
+          </TouchableOpacity>
         </View>
         <FlatList
           data={filteredChats.slice().reverse()}
           renderItem={renderChatItem}
-          keyExtractor={(item) => item.id}
+          keyExtractor={(item) => item.id.toString()} // Use the id property as the key
           contentContainerStyle={styles.messageList}
         />
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => setModalVisible(false)}
+        >
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Text style={styles.modalTitle}>Select Modules</Text>
+              <ScrollView style={styles.moduleList}>
+                <View style={styles.moduleOptionsContainer}>
+                  {moduleOptions.map((moduleCode) => (
+                    <TouchableOpacity
+                      key={moduleCode}
+                      style={[
+                        styles.moduleOption,
+                        tempSelectedModules.includes(moduleCode) && styles.moduleOptionSelected,
+                      ]}
+                      onPress={() => {
+                        const updatedModules = tempSelectedModules.includes(moduleCode)
+                          ? tempSelectedModules.filter((c) => c !== moduleCode)
+                          : [...tempSelectedModules, moduleCode];
+                        setTempSelectedModules(updatedModules);
+                      }}
+                    >
+                      <Text
+                        style={[
+                          styles.moduleOptionText,
+                          tempSelectedModules.includes(moduleCode) && styles.moduleOptionTextSelected,
+                        ]}
+                      >
+                        {moduleCode}
+                      </Text>
+                      {tempSelectedModules.includes(moduleCode) ? (
+                        <AntDesign name="checkcircle" size={20} color="#009688" />
+                      ) : (
+                        <AntDesign name="checkcircleo" size={20} color="#9e9e9e" />
+                      )}
+                    </TouchableOpacity>
+                  ))}
+                </View>
+              </ScrollView>
+              <TouchableOpacity style={styles.applyButton} onPress={applyFilter}>
+                <Text style={styles.applyButtonText}>Apply</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
         <BottomBar navigation={navigation} />
       </SafeAreaView>
     );
@@ -215,6 +297,15 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     borderBottomColor: '#ccc',
     padding: 16,
+  },
+  filterButton: {
+    backgroundColor: '#009688',
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 3,
   },
   innerHeader: {
     flexDirection: 'row',
@@ -293,6 +384,70 @@ const styles = StyleSheet.create({
     color: '#666',
     marginBottom: 4,
   },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalHeader: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+  modalContent: {
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    padding: 20,
+    width: '80%',
+    alignItems: 'center',
+  },
+  modalTitleSubmit: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  moduleList: {
+    width: '100%',
+    maxHeight: 200,
+    marginBottom: 20,
+  },
+  moduleOption: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  moduleOptionSelected: {
+    backgroundColor: '#f5f5f5',
+    borderRadius: 5,
+  },
+  moduleOptionText: {
+    marginLeft: 5,
+    marginRight: 'auto',
+  },
+  moduleOptionTextSelected: {
+    fontWeight: 'bold',
+    color: '#009688',
+  },
+  applyButton: {
+    backgroundColor: '#009688',
+    borderRadius: 5,
+    marginTop: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  applyButtonText: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
   messageInputContainer: {
     borderTopWidth: 1,
     padding: 3,
@@ -330,17 +485,17 @@ const styles = StyleSheet.create({
   searchContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 0,
-    paddingLeft: 6,
+    paddingHorizontal: 7,
+    marginBottom: 10,
   },
   searchInput: {
     flex: 1,
+    height: '100%',
     borderWidth: 1,
-    borderColor: '#333',
-    borderRadius: 8,
-    paddingVertical: 8,
+    borderColor: '#999999',
+    borderRadius: 20,
     paddingHorizontal: 12,
-    marginRight: 8,
+    marginRight: 10,
   },
 });
 
