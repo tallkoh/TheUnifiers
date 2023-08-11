31c76f2bd0dad37a74ee20ee832ec00f03672b6b
tallkoh <xnkohs@gmail.com>
2023-08-11 11:45:59 +0800

    Cleared up backend

--- backend/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-BASE = "https://unified-nus-2b9aee59f9d9.herokuapp.com/"
-
-response = requests.post(BASE + "channels/nuswomeninbusiness")
-
-print(response.status_code)
-print(response.content)
